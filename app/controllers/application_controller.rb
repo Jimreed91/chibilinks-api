74df4fd2f70ab93b7d227e9e065cfa0b4a5e4f27
James Reed <jamesreed.3050@gmail.com>
2022-09-27 23:44:49 +0900

    adding home route to front, cors tightened

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::API
+
+  def home
+    redirect_to "https://chibilinks.com"
+  end
 end
--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -28,9 +28,6 @@ class LinksController < ApplicationController
 
     end
   end
-  def healthy
-   render json: { status: 200 }
-  end
 
   private
 
