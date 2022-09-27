51e7f1ab28215a32024bbe0d6d6801c017f028b2
James Reed <jamesreed.3050@gmail.com>
2022-09-28 00:09:30 +0900

    cors get - open

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -11,6 +11,13 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
 
     resource '*',
       headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      methods: [:get, :post, :options, :head]
+  end
+
+  allow do
+    origins '*'
+    resource '*',
+    headers: :any,
+    methods: :get
   end
 end
