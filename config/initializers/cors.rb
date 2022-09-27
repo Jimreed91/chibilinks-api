0007cae48de69636e144872599c07c82b052463a
James Reed <jamesreed.3050@gmail.com>
2022-09-27 23:58:48 +0900

    slight cors change

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://www.chibilinks.com/'
+    origins 'https://www.chibilinks.com'
 
     resource '*',
       headers: :any,
